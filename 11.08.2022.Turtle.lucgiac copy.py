5798cec43e17b53664760705d94fe820e8a89ce1
Nhan-L <lenguyenvietnhan96@gmail.com>
2022-08-16 11:20:17 +0700

    3 hinh tron dong tam

---
+++ 11.08.2022.Turtle.lucgiac copy.py
@@ -0,0 +1,5 @@
+ten = "lenguyenvietnhan"
+tuoi = 26
+chieucao = 171 
+cannang = 76
+print (ten, tuoi, chieucao, cannang)
--- demo2.py
+++ demo2.py
@@ -1,1 +1,30 @@
-print (1234567)
+# Hinh tron dong tam
+from turtle import clear, pencolor, penup
+khoangcach = 20
+chuvi1 = 50
+chuvi2 = chuvi1 + khoangcach
+chuvi3 = chuvi2 + khoangcach
+import turtle
+nhan_screen = turtle.Screen()            
+nhan_screen = turtle.Turtle()  
+nhan_screen.speed(5)
+
+nhan_screen.circle(chuvi1)
+nhan_screen.right(90)
+
+nhan_screen.penup()
+nhan_screen.goto(0,-20)
+nhan_screen.pendown()
+
+nhan_screen.left(90)
+nhan_screen.circle(chuvi2)
+nhan_screen.right(90)
+
+nhan_screen.penup()
+nhan_screen.goto(0,-40)
+nhan_screen.pendown()
+
+nhan_screen.left(90)
+nhan_screen.circle(chuvi3)
+
+turtle.done
