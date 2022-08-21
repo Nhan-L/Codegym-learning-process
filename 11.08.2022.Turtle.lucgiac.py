6663648c6e7218ee35e5307847c9b52696f83964
Nhan-L <lenguyenvietnhan96@gmail.com>
2022-08-21 10:43:48 +0700

    tao thong tin ca nhan

---
+++ 11.08.2022.Turtle.lucgiac.py
@@ -0,0 +1,21 @@
+from turtle import clear
+
+
+chieudai = 100
+gocxoay = 60
+import turtle
+nhan_screen = turtle.Screen()            
+nhan_screen = turtle.Turtle()    
+nhan_screen.forward(chieudai)                    
+nhan_screen.left(gocxoay)
+nhan_screen.forward(chieudai)                    
+nhan_screen.left(gocxoay)
+nhan_screen.forward(chieudai)                    
+nhan_screen.left(gocxoay)
+nhan_screen.forward(chieudai)                    
+nhan_screen.left(gocxoay)
+nhan_screen.forward(chieudai)                    
+nhan_screen.left(gocxoay)
+nhan_screen.forward(chieudai)                    
+nhan_screen.left(gocxoay)
+turtle.done
